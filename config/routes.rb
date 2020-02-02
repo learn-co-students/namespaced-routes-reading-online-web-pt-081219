d317a4ee3f68d21538bc7b13fe5132b430ccb2d2
Christian Cain <christian22cain@yahoo.com>
2020-02-02 12:36:30 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,18 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
+  #Hardcoded route -- stats_path
+  #get '/admin/stats', to: 'stats#index'
+
+  #Scoped route -- stats_path
+  # scope '/admin', module: 'admin' do
+  #   resources :stats, only: [:index]
+  # end
+
+  #Namespaced route -- admin_stats_path
+  namespace :admin do
+    resources :stats, only: [:index]
+  end
 
   root 'posts#index'
 end
